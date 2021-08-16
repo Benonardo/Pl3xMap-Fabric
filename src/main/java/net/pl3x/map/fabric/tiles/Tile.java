7d6f5c8ac8a6255e0dec8080dc109c934ca36b9c
William Blake Galbreath <blake.galbreath@gmail.com>
2021-08-16 07:41:44 -0500

    Fix directories containing `:` (replace with `-`)

--- src/main/java/net/pl3x/map/fabric/tiles/Tile.java
+++ src/main/java/net/pl3x/map/fabric/tiles/Tile.java
@@ -131,7 +131,7 @@ public class Tile {
     }
 
     public File getFile() {
-        File dir = new File(new File(new File(Constants.MODID, ip()), this.world.getName()), String.valueOf(this.zoom));
+        File dir = new File(new File(new File(Constants.MODID, ip().replace(":", "-")), this.world.getName()), String.valueOf(this.zoom));
         if (!dir.exists()) {
             if (!dir.mkdirs()) {
                 throw new IllegalStateException("Cannot create tiles directory for " + this.world + " Directory: " + dir.getAbsolutePath());
